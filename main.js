f13574f3e0a68c1264a4b9d9f6aa9800d119cdac
ヒチヒチ <90025466+hclhcl7@users.noreply.github.com>
2023-06-13 16:41:19 +0900

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let text1 = "";
 let textConte = "";
 let text2 = "To Do: Fix collisions, Fix spider legs movement";
 let text3 = "made by AstRatJP";
-let text4 = "ver 1.7";
+let text4 = "ver 1.7.1";
 
 let historyX = [];
 let historyY = [];
